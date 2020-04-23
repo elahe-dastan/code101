45483723b983b52d9b81e230d2fabc3651aaa800
Elahe Dastan <elahe.dstn@gmail.com>
2020-04-24 00:34:05 +0430

    fix: Correct White Sequare Problem

--- Go/white_square/main.go
+++ Go/white_square/main.go
@@ -7,59 +7,56 @@ import (
 	"strings"
 )
 
+type xy struct {
+	x, y int
+}
 
 func main() {
 	var n, m, k int
-	fmt.Scanf("%d %d", &n, &m)
-	fmt.Scanf("%d", &k)
 
-	if k < m * m {
+	reader := bufio.NewReader(os.Stdin)
+
+	l1, _ := reader.ReadString('\n')
+	fmt.Sscanf(l1, "%d %d", &n, &m)
+
+	l2, _ := reader.ReadString('\n')
+	fmt.Sscanf(l2, "%d", &k)
+
+	if k < m*m {
 		fmt.Println("No")
 		return
 	}
 
-	matrix := make([][]int, n)
-
-	for i := 0; i < n; i++ {
-		row := make([]int, n)
-		matrix[i] = row
-	}
-
-	reader := bufio.NewReader(os.Stdin)
-
+	matrix := make(map[xy]bool)
 
 	for i := 0; i < k; i++ {
-		l,_ := reader.ReadString('\n')
+		l, _ := reader.ReadString('\n')
 		l = strings.TrimSuffix(l, "\n")
 		var x, y int
-		fmt.Sscanf(l,"%d %d", &x, &y)
+		fmt.Sscanf(l, "%d %d", &x, &y)
 
-		matrix[x-1][y-1] = 1
+		point := xy{x - 1, y - 1}
+
+		matrix[point] = true
 	}
 
 	fmt.Println(find(matrix, m))
 }
 
-func find(matrix [][]int, m int) string {
-	for i := 0; i <= len(matrix)-m; i++ {
-		for j := 0; j <= len(matrix)-m; j++ {
-			if matrix[i][j] == 0 {
-				continue
-			}
-
-			if check(matrix, i, j, m) {
-				return "Yes"
-			}
+func find(matrix map[xy]bool, m int) string {
+	for point := range matrix {
+		if check(matrix, point.x, point.y, m) {
+			return "Yes"
 		}
 	}
 
 	return "No"
 }
 
-func check(matrix [][]int, i int, j int, m int) bool {
-	for x := i; x < i + m; x++ {
-		for y := j; y < j + m; y++ {
-			if matrix[x][y] == 0 {
+func check(matrix map[xy]bool, i int, j int, m int) bool {
+	for x := i; x < i+m; x++ {
+		for y := j; y < j+m; y++ {
+			if !matrix[xy{x, y}] {
 				return false
 			}
 		}
