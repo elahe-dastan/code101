cf1e8e7cfb865e01aedab9a28eef7fffbfe65609
elahe dastan <elahe.dstn@gmail.com>
2021-01-17 18:23:29 +0330

    it works

--- Go/koanf/main.go
+++ Go/koanf/main.go
@@ -26,13 +26,23 @@ func main() {
 	// koanf.Provider is a generic interface that provides configuration , for example, from files, environment variables,...
 	// koanf.Parser is a generic interface that takes raw bytes, parses, and returns a nested map[string]interface{} representation.
 	f := file.Provider("Go/koanf/config.yml")
+
 	if err := k.Load(f, yaml.Parser()); err != nil {
 		log.Fatal(err)
 	}
 
+	var out Elahe
+	if err := k.Unmarshal("elahe", &out); err != nil{
+		log.Fatal(err)
+	}
+
+	fmt.Println(out)
+
+
 	// Watching files for changes. The most important advantage of koanf over viper
 	// Watch the file and get a callback on change. The callback can do whatever,
 	// like re-load the configuration.
+	//fuck(f, out)
 	f.Watch(func(event interface{}, err error) {
 		if err != nil {
 			log.Fatal(err)
@@ -41,19 +51,25 @@ func main() {
 		if err := k.Load(f, yaml.Parser()); err != nil {
 			log.Fatal(err)
 		}
+
+		if err := k.Unmarshal("elahe", &out); err != nil{
+			log.Fatal(err)
+		}
+
+		fmt.Println(out)
+
 	})
 
-	var out Elahe
-	if err := k.Unmarshal("", &out); err != nil{
-		log.Fatal(err)
-	}
-
-	check(out)
-
+	//block forever
+	check()
 }
 
-func check(out Elahe) {
-	fmt.Println(out)
+func check() {
 	//block forever
 	<-make(chan bool)
 }
+
+// points
+// relative paths are based on where your go.mod is
+// the delimiter is used for addressing for example elahe.name="raha"
+// if you want to unmarshal into a struct, its fields should be exported
