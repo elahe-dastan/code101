b8b29af122164b4b14eb495bda00124df161a883
Parham Alvani <parham.alvani@gmail.com>
2021-07-06 23:49:52 +0430

    feat: Add Curl Example

--- Go/echo/echo.go
+++ Go/echo/echo.go
@@ -34,3 +34,4 @@ func eat(c echo.Context) error {
 // In this piece of code I want to test two things
 // one: the tag code works
 // two: a struct in another one is bound by echo properly
+// curl -X POST -d '{"name": "parham"}' -H 'Content-Type: application/json' '127.0.0.1:1373?type=panda&love=raha'
